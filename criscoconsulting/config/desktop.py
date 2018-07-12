b956023a187e44a6551d63a49e3db10a064b43e0
erpnextdeveloper <erpnextdeveloper1@gmail.com>
2018-07-13 00:10:44 +0530

    first commit

---
+++ criscoconsulting/config/desktop.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "criscoconsulting",
+			"color": "grey",
+			"icon": "fa fa-life-ring",
+			"type": "module",
+			"label": _("KSATB")
+		}
+	]
